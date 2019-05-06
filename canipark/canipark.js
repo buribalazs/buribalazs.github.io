483eb82bcc656134af465f411efd499f27c4a837
Buri Balázs <balazs.buri@codeandsoda.hu>
2019-05-06 15:12:58 +0200

    canipark init

---
+++ canipark/canipark.js
@@ -0,0 +1,31 @@
+(function(){
+    const video = document.querySelector('video');
+    const canvas = window.canvas = document.querySelector('canvas');
+
+
+    canvas.width = 480;
+    canvas.height = 360;
+    
+    const button = document.querySelector('.camera-container');
+    button.onclick = function() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    };
+    
+    const constraints = {
+        audio: false,
+        video: { facingMode: "environment" }
+    };
+    
+    function handleSuccess(stream) {
+        window.stream = stream; // make stream available to browser console
+        video.srcObject = stream;
+    }
+    
+    function handleError(error) {
+        console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
+    }
+    
+    navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+})();
