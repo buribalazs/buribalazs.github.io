7c78fe5656834a4f9c5c099d942f8df773b28aaa
Buri Balázs <balazs.buri@codeandsoda.hu>
2019-05-08 10:48:04 +0200

    add processing screen

--- canipark/canipark.js
+++ canipark/canipark.js
@@ -5,6 +5,18 @@
 
     canvas.width = 480;
     canvas.height = 360;
+
+    const cameraScreen = document.querySelector('.camera-app-container-inner.camera');
+    const processingScreen = document.querySelector('.camera-app-container-inner.process');
+
+    const screens = [cameraScreen, processingScreen];
+
+    function show(screen) {
+        screens.forEach(s => s.classList.add('camera-screen-hidden'));
+        screen.classList.remove('camera-screen-hidden')
+    }
+
+    show(cameraScreen);
     
     const app = document.querySelector('.camera-app-container');
     const button = document.querySelector('.camera-container');
@@ -12,6 +24,14 @@
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        setTimeout(() => {
+            show(processingScreen);
+        }, 1000);
+
+        setTimeout(() => {
+            window.location = 'http://www.canipark.com.au/p10EE3';
+        }, 2000);
     };
     
     const constraints = {
@@ -21,7 +41,6 @@
     
     function handleSuccess(stream) {
         app.classList.add('initialized');
-        window.stream = stream; // make stream available to browser console
         video.srcObject = stream;
     }
     
