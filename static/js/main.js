82a42dad453c1b1f179c889a550f782f35e52635
Buri Balázs <balazs.buri@codeandsoda.hu>
2020-03-08 17:40:33 +0100

    update

--- static/js/main.js
+++ static/js/main.js
@@ -12,4 +12,23 @@
 
     window.openLogin = openLogin
     window.closeLogin = closeLogin
+
+    //webstore
+    //custom name
+    Array.prototype.slice.call(document.querySelectorAll('.product-card__custom-name')).forEach(parent => {
+        let input = parent.querySelector('input')
+        let label = parent.querySelector('.product-card__custom-name__label')
+        parent.querySelector('.product-card__custom-name__pencil').addEventListener('click', e => {
+            e.preventDefault()
+            parent.classList.add('product-card__custom-name--active')
+            input.focus()
+        })
+        function saveChange(e){
+            e.preventDefault()
+            label.textContent = input.value
+            parent.classList.remove('product-card__custom-name--active')
+        }
+        parent.querySelector('.product-card__custom-name__save').addEventListener('click', saveChange)
+        input.addEventListener('change', saveChange)
+    });
 })()
